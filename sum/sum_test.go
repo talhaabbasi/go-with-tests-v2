b615a8d928b2f1e66da302bf8e919b4281c880e1
Talha Abbasi <talhaabbasi1997@gmail.com>
2024-07-12 02:26:44 +0500

    feat: add tests for empty slices :muscle:

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -19,13 +19,24 @@ func TestSum(t *testing.T) {
 }
 
 func TestSumAllTails(t *testing.T) {
-	t.Run("collection of any size", func(t *testing.T) {
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+	
+	t.Run("make the sums of some slices", func(t *testing.T) {
 		got := SumAllTails([]int{1,2,3}, []int{0, 9})
 		want := []int{5, 9}
 
-		if !slices.Equal(got, want) {
-			t.Errorf("got %v but want %v", got, want)
-		}
+		checkSums(t, got, want)
+	})
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
 	})
 }
 
