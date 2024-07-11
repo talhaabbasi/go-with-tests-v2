b7c1831de4cab5037357b5ced0814185a39f7f3b
Talha Abbasi <talhaabbasi1997@gmail.com>
2024-07-12 02:10:53 +0500

    fix: failing sum test :bug:

--- sum/sum.go
+++ sum/sum.go
@@ -8,8 +8,7 @@ func Sum (numbers []int) (sum int) {
 }
 
 func SumAll (numbersToSum ...[]int) []int {
-	lengthOfNumbers := len(numbersToSum)
-	sums := make([]int, lengthOfNumbers)
+	var sums []int
 
 	for _, numbers := range numbersToSum {
 		sums = append(sums, Sum(numbers))
