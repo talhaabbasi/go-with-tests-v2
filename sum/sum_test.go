1e7f6edeaebfc2f47c61e30161e99b73da5591d5
Talha Abbasi <talhaabbasi1997@gmail.com>
2024-07-12 02:13:48 +0500

    feat: add SumAllTails function :muscle:

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -18,10 +18,10 @@ func TestSum(t *testing.T) {
 	})
 }
 
-func TestSumAll(t *testing.T) {
+func TestSumAllTails(t *testing.T) {
 	t.Run("collection of any size", func(t *testing.T) {
-		got := SumAll([]int{1,2,3}, []int{0, 9})
-		want := []int{6, 9}
+		got := SumAllTails([]int{1,2,3}, []int{0, 9})
+		want := []int{5, 9}
 
 		if !slices.Equal(got, want) {
 			t.Errorf("got %v but want %v", got, want)
