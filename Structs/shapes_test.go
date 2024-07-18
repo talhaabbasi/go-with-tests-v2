16c348b871d773a48b10f5b823f3b5ae47b7f9eb
Talha Abbasi <talhaabbasi1997@gmail.com>
2024-07-18 15:24:44 +0500

    feat: add structs and interfaces :muscle:

--- Structs/shapes_test.go
+++ Structs/shapes_test.go
@@ -3,7 +3,7 @@ package structs
 import "testing"
 func TestPerimeter (t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
-	got := Perimeter(rectangle)
+	got := rectangle.Perimeter()
 	want := 40.0
 
 	if got != want {
@@ -12,11 +12,20 @@ func TestPerimeter (t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Area(rectangle) 
-	want := 100.0
-
-	if got != want {
-		t.Errorf(" got %.2f want %.2f", got, want)
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf(" got %g want %g", got, want)
+		}
 	}
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		checkArea(t, rectangle, 100)
+	})
+
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		checkArea(t, circle, 314.1592653589793)
+	})
 }
