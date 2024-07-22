0dde7427ce24faaddb255db8ea2a8fa3927f803b
Talha Abbasi <talhaabbasi1997@gmail.com>
2024-07-22 12:08:37 +0500

    feat: add struct to shapes :muscle:

--- Structs/shapes_test.go
+++ Structs/shapes_test.go
@@ -12,20 +12,22 @@ func TestPerimeter (t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf(" got %g want %g", got, want)
-		}
+	areaTests := []struct {
+		name string
+		shape Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{10,10}, hasArea: 100},
+		{name: "Circle", shape: Circle{10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{12, 6}, hasArea: 36.0},
 	}
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		checkArea(t, rectangle, 100)
-	})
 
-	t.Run("circle", func(t *testing.T) {
-		circle := Circle{10.0}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea )
+			}
+		})
+	}
 }
